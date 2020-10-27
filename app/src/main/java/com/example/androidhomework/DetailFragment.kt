60e1a9234c59912ae83647687c6ed316db001e37
kocoukot <kocou-kot@yandex.ru>
2020-10-27 18:41:35 +0400

    module 14

---
+++ app/src/main/java/com/example/androidhomework/DetailFragment.kt
@@ -0,0 +1,40 @@
+package com.example.androidhomework
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+
+class DetailFragment : Fragment() {
+
+
+
+    companion object {
+        var text = ""
+
+        fun newInstance(text: String): DetailFragment{
+            this.text = text
+            val fragment = DetailFragment()
+            return fragment
+        }
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        detailInfo.text = ("info about $text")
+    }
+
+}
---
+++ app/src/main/java/com/example/androidhomework/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.androidhomework
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_test.*
+import kotlinx.android.synthetic.main.main_fragment.*
+
+
+class MainFragment : Fragment(R.layout.main_fragment) {
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        parentFragmentManager.beginTransaction().add(R.id.main_Fragment, ListFragment()).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
